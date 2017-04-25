57a1d0bcf52094d05918e5145cd55aa3fe629ec0
Yar01av <Yar01av@users.noreply.github.com>
2017-04-25 23:11:09 +0200

    Update Main.py

--- Date_Countdown/Main.py
+++ Date_Countdown/Main.py
@@ -36,7 +36,7 @@ def string_dates_to_diffs(year_str, month_str, day_str, current_date):
 	return (event_date - current_date)
 
 for i in range(0, int(len(saved_lines_of_dates)/4)):
-	if len(saved_lines_of_dates) > 1:
+	if len(saved_lines_of_dates) > 0:
 		date_diff = string_dates_to_diffs(saved_lines_of_dates[4*i + 1], saved_lines_of_dates[4*i + 2], saved_lines_of_dates[4*i + 3], current_date)
 		print(saved_lines_of_dates[4*i] + str(date_diff.days) + " days.\n")
 
@@ -61,9 +61,18 @@ while True:
 	to_save_switch = input("Would you like to save this date. Then, next time you run the program, the number of days will be culculated automatically. To save the date, enter 's'. Otherwise, enter anything.\n")
 	if to_save_switch == "s":
 		date_description = input("How do you wish to name your date?\n")
-		with open("Saved_dates.txt", "a") as saved_dates:
-			saved_dates.write(date_description + "\n" + year_string + "\n" + month_string + "\n" + day_string + "\n")
+		with open("Saved_dates.txt", "r+") as saved_dates:
+			if len(saved_dates.readlines()) > 0:
+				is_empty_file = False
+			else:
+				is_empty_file = True
 
+		if is_empty_file == True:
+			with open("Saved_dates.txt", "w") as saved_dates:
+				saved_dates.write(date_description + "\n" + year_string + "\n" + month_string + "\n" + day_string + "\n")
+		else:
+			with open("Saved_dates.txt", "a") as saved_dates:
+				saved_dates.write(date_description + "\n" + year_string + "\n" + month_string + "\n" + day_string + "\n")
 		print("Your date was successfully saved! You will see the countdown the next time you open the program.\n")
 
 	time.sleep(2)
